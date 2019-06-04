5649d2873ece9a8984613210f983f02982ac45ce
oTrinhHoangHai <trinh.hoang.hai@framgia.com>
2019-06-04 08:26:18 +0700

    Initial Commit

---
+++ MesDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MesDemo
+//
+//  Created by trinh.hoang.hai on 6/4/19.
+//  Copyright © 2019 trinh.hoang.hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
